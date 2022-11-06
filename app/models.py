d43244db1f69033fbcc13e0fc2cf5c005ccd1577
Brigham Andersen <brighamband@gmail.com>
2022-11-05 21:47:52 -0600

    First stab at making table

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100, blank=False, null=False)
+    # user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # resumeUrl = models.CharField(max_length=200)
+    # targetSalaryMin = models.IntegerField(required=True)
+    # targetSalaryMax = models.IntegerField()
+    # topBenefits = models.CharField(max_length=200, required=True)  # Comma-separated string array
+    # certifications = models.CharField(max_length=200)  # Comma-separated string array
+    # topTraits = models.CharField(max_length=200, required=True)  # Comma-separated string array
+    # additionalInfo = models.TextField()
--- app/views.py
+++ app/views.py
@@ -1,28 +1,27 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponseBadRequest, HttpResponse, HttpResponseRedirect
-from django import forms
-from django.urls import reverse
-from django.views.decorators.csrf import csrf_exempt
-
-# forms
-class EmployeeForm(forms.Form):
-    first_name = forms.CharField(label="First Name", max_length=100, required=True)
+from django.http import HttpResponseBadRequest
+from .models import Employee
 
 
 def index(request):
     return render(request, "index.html")
 
 
-# FIXME below, should remove
-@csrf_exempt
 def apply(request):
+    if request.method != "POST":
+        return HttpResponseBadRequest("This endpoint only allows POST")
+    first_name = request.POST.get("first_name")
+    middle_name = request.POST.get("middle_name")
+    last_name = request.POST.get("last_name")
+
+    employee = Employee()
+    employee.first_name = first_name
+    employee.save()
     return redirect(success)
     # first_name = request.POST["first_name"] # .get("first_name")
     # last_name = request.POST["last_name"]
 
     # return HttpResponse(first_name + " " + last_name)
-    # if request.method != "POST":
-    #     return HttpResponseBadRequest("This endpoint only allows POST")
 
     # context = {"first_name": ""}
     # print("validating")
